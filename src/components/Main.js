0b79121bf35878a301c3b08078dd9f9a4389eb3d
Tim Traylor <ttraylor83@gmail.com>
2022-08-23 01:22:11 -0700

    working lat and long for the form

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,19 +1,61 @@
 import React from 'react';
+import axios from 'axios';
 
 class Main extends React.Component{
+  constructor (props) {
+    super(props);
+    this.state = {
+      city: '',
+      cityData: [],
+    }
+  }
 
-  render (){
+  handleInput = (e) => {
+    e.preventDefault();
+    this.setState ({
+      city: e.target.value
+    })
+  }
+
+  getCityData = async (e) => {
+    e.preventDefault();
+
+    let url = `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json`
+    let cityData = await axios.get(url);
+    this.setState({
+      cityData: cityData.data
+    })
+  }
+
+
+  render () {
+
+    let nameData = this.state.cityData.map( (val, idx) => {
+      return <p key={idx}>Name: {val.display_name}</p>
+    })
+    let nameData2 = nameData[0];
+
+    let locDataDone = this.state.cityData.map( (val, idx) => {
+      return <li key={idx}>Latitude: {val.lat}; Longitude: {val.lon}</li>
+    })
+    let locDataDone2 = locDataDone[0];
+
+
     return(
       <>
         <main>
-          <form>
-            <input>
-            </input>
-            <button>Report Sir.</button>
+          <form onSubmit={this.getCityData}>
+            <label>
+              <input type="text" onInput={this.handleInput} />
+            </label>
+            <button type='submit'>Explore!</button>
           </form>
         </main>
         <section>
-          <p>Lists of Geolocation information from API</p>
+            {nameData2}
+          <ul>
+            {locDataDone2}
+          </ul>
         </section>
         <article>
           <p>Location of map? Card?</p>
